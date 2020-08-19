0003280d11061da94d0f5e3e3660f6e746fe9900
u394 <69897452+u394@users.noreply.github.com>
2020-08-19 15:35:26 +0500

    Add files via upload

---
+++ Test2.js
@@ -0,0 +1,12 @@
+function myFunction() {
+  var a = location.hash;
+  var b = location.host;
+  var c = location.hostname;
+  var d = location.href;
+  var e = location.pathname;
+  var f = "Port: " + location.port;
+  var g = location.protocol;
+  var h = location.search;
+    location.assign("http://127.0.0.1:5500/Test1.html");
+  
+  }
